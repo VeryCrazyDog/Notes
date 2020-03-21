4a5ad65d84c5bbbbd61beef39e7dfe735faad1d2
VCD <VeryCrazyDog@gmail.com>
2020-03-21 14:36:38 +0800

    Enhance template

--- Computer/Node.js/Templates/loglevel-logger.js
+++ Computer/Node.js/Templates/loglevel-logger.js
@@ -1,12 +1,32 @@
+'use strict'
+
+// Include 3rd party modules
 const loglevel = require('loglevel')
 const loglevelPrefix = require('loglevel-plugin-prefix')
 
+// Build level description mapping
+const levelDescriptions = Object.entries(loglevel.levels).reduce((accumulator, [desc, index]) => {
+  accumulator[index] = desc
+  return accumulator
+}, [])
+
+// Module initialization
 loglevel.setDefaultLevel('info')
 loglevelPrefix.reg(loglevel)
 loglevelPrefix.apply(loglevel, {
   template: '%t [%l]',
   timestampFormatter: date => date.toISOString()
 })
+
+// Apply our log level
+if (process.env.LOG_LEVEL) {
+  loglevel.setLevel(process.env.LOG_LEVEL)
+}
+
+// Add extra function
+loglevel.getLevelDescription = function () {
+  return levelDescriptions[this.getLevel()]
+}
 
 module.exports = loglevel
 
